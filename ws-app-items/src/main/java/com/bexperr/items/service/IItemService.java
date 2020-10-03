da7ebd02bff54d081719f2078b3746640124aeec
bexperrr <sesilu.id@gmail.com>
2020-10-02 20:54:46 -0500

    Crud REST Template
    Implementando Crud Sobre ITEMS

--- ws-app-items/src/main/java/com/bexperr/items/service/IItemService.java
+++ ws-app-items/src/main/java/com/bexperr/items/service/IItemService.java
@@ -7,5 +7,11 @@ public interface IItemService {
 	
 	public List<Item> findAll();
 	public Item findById(Long id, Integer cantidad);
+	
+	public Producto save (Producto producto);
+	public Producto update (Producto producto, Long id);
+	public void delete(Long id);
+	
+	
 
 }
--- ws-app-items/src/main/java/com/bexperr/items/service/implement/ItemServiceFeing.java
+++ ws-app-items/src/main/java/com/bexperr/items/service/implement/ItemServiceFeing.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.bexperr.items.clientes.ProductoRest;
 import com.bexperr.items.models.Item;
+import com.bexperr.items.models.Producto;
 import com.bexperr.items.service.IItemService;
 
 @Service("serviceFeign")
@@ -25,6 +26,24 @@ public class ItemServiceFeing implements IItemService{
 	@Override
 	public Item findById(Long id, Integer cantidad) {
 		return new Item(clienteFeign.detalle(id),cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
