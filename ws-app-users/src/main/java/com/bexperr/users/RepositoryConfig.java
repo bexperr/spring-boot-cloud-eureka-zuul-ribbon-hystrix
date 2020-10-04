0032e2be736520080ca4d89a787c943541dbf3ac
bexperrr <sesilu.id@gmail.com>
2020-10-03 21:14:47 -0500

    CRUD, Agregando Models al Commons

---
+++ ws-app-users/src/main/java/com/bexperr/users/RepositoryConfig.java
@@ -0,0 +1,17 @@
+package com.bexperr.users;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.bexperr.users.models.Role;
+import com.bexperr.users.models.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer{
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+}
--- ws-app-users/src/main/java/com/bexperr/users/dao/UsuarioDao.java
+++ ws-app-users/src/main/java/com/bexperr/users/dao/UsuarioDao.java
@@ -2,10 +2,17 @@ package com.bexperr.users.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import com.bexperr.users.models.Usuario;
+
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 	
-	public Usuario findByUsername(String username);
+	@RestResource(path="buscar-username")
+	public Usuario findByUsername(@Param("username") String username);
 	
 //	@Query(value="select u from Usuario u where u.username=?1",nativeQuery = true)
 	@Query(value="select u from Usuario u where u.username=?1")
